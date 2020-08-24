278d5cc972110ffadaa5bfd591196e6e5b7912c0
vancantmemory <derekjunhao@gmail.com>
2020-08-24 13:29:04 +0800

    mistake

--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -120,10 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'resources/images'),
     os.path.join(BASE_DIR, 'resources/JS'),
     os.path.join(BASE_DIR, 'resources/CSS'),
 ]
+
 
 
---
+++ mall/mall/test.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from ISI.models import product
+
+# class ProductModelTests(TestCase):
+#
+#     def test_product_list(self):
+#
+
